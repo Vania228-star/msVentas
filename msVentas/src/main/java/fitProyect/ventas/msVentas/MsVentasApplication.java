c9b8fd7ebc9598285bef156a58a567f5a13c52ec
Vania <Van.carvajal@duocuc.cl>
2026-05-15 16:40:25 -0400

    first commit

---
+++ msVentas/src/main/java/fitProyect/ventas/msVentas/MsVentasApplication.java
@@ -0,0 +1,13 @@
+package fitProyect.ventas.msVentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVentasApplication.class, args);
+	}
+
+}
