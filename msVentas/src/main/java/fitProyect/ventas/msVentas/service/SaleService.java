98b4793ee70d6acab8ecdebc3c0d5d16d222d659
Vania <Van.carvajal@duocuc.cl>
2026-05-15 16:57:49 -0400

    service

--- msVentas/src/main/java/fitProyect/ventas/msVentas/service/SaleService.java
+++ msVentas/src/main/java/fitProyect/ventas/msVentas/service/SaleService.java
@@ -1,5 +1,29 @@
 package fitProyect.ventas.msVentas.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import fitProyect.ventas.msVentas.model.Sale;
+import fitProyect.ventas.msVentas.repository.SaleRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class SaleService {
+
+    private final SaleRepository saleRepository;
+
+    public List<Sale> findAll(){
+        return saleRepository.findAll();
+    }
+
+    public Sale save(Sale sale){
+        return saleRepository.save(sale);
+    }
+
+    public Sale findById(String id){
+        return saleRepository.findById(id).orElse(null);
+    }
     
 }
